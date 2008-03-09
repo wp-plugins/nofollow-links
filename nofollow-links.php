e54f128c2043ef19aed7015c52b7d54b1081e588
shellab <shellab@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-03-09 21:45:36 +0000

    tagging version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/nofollow-links/tags/1.0.1@34229 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ nofollow-links.php
@@ -0,0 +1,133 @@
+<?php
+/*
+Plugin Name: Nofollow Links
+Plugin URI: http://blog.andrewshell.org/nofollow-links/
+Description: Select which links in your blogroll you want to nofollow.
+Version: 1.0.1
+Author: Andrew Shell
+Author URI: http://blog.andrewshell.org/
+
+Copyright (c) 2008 Andrew Shell
+Released under the GNU General Public License (GPL)
+http://www.gnu.org/licenses/gpl.txt
+*/
+
+// Define
+if (!function_exists('array_combine')) {
+    require_once dirname(__FILE__) . '/array_combine.php';
+
+    function array_combine($keys, $values)
+    {
+        return php_compat_array_combine($keys, $values);
+    }
+}
+
+add_action('admin_menu',    'nofollow_links_admin_menu');
+add_filter('get_bookmarks', 'nofollow_links_get_bookmarks', 10, 2);
+
+function nofollow_links_admin_menu()
+{
+    add_submenu_page('link-manager.php', 'Nofollow Links', 'Nofollow Links', 10, 'link-nofollow', 'nofollow_links_manage');
+}
+
+function nofollow_links_manage()
+{
+    if (isset($_POST['nofollowbookmarks'])) {
+        check_admin_referer('nofollow_links_manage');
+
+        if (isset($_POST['linkcheck']) && is_array($_POST['linkcheck'])) {
+            $nofollow = array_combine($_POST['linkcheck'], $_POST['linkcheck']);
+        } else {
+            $nofollow = array();
+        }
+        update_option('nofollow_links', serialize($nofollow));
+
+        echo '<div style="background-color: rgb(207, 235, 247);" id="message" class="updated fade"><p>' . count($nofollow) . ' links marked nofollow.</p></div>' . "\n";
+    }
+
+    $sNofollowLinks = get_option("nofollow_links");
+    if (!$sNofollowLinks) {
+        $sNofollowLinks = serialize(array());
+    }
+    $uNofollowLinks = unserialize($sNofollowLinks);
+
+    $links = get_bookmarks();
+    ?>
+    <script type="text/javascript">
+    <!--
+    function checkAll(form)
+    {
+        for (i = 0, n = form.elements.length; i < n; i++) {
+            if(form.elements[i].type == "checkbox") {
+                if(form.elements[i].checked == true)
+                    form.elements[i].checked = false;
+                else
+                    form.elements[i].checked = true;
+            }
+        }
+    }
+    //-->
+    </script>
+
+    <div class="wrap">
+
+    <h2>Nofollow Links</h2>
+
+    <form id="links" name="pages-form" action="link-manager.php?page=link-nofollow" method="post">
+    <?php if (function_exists('wp_nonce_field')) { wp_nonce_field('nofollow_links_manage'); } ?>
+    <table class="widefat">
+    <thead>
+    <tr>
+        <th width="45%">Name</th>
+        <th>URL</th>
+        <th style="text-align: center"><input type="checkbox" onclick="checkAll(document.getElementById('links'));" /></th>
+    </tr>
+    </thead>
+    <tbody><?php
+    $alt = false;
+    foreach ($links as $link) {
+        $short_url = str_replace('http://', '', $link->link_url);
+        $short_url = str_replace('www.', '', $short_url);
+        if ('/' == substr($short_url, -1)) {
+            $short_url = substr($short_url, 0, -1);
+        }
+        if (strlen($short_url) > 35) {
+            $short_url = substr($short_url, 0, 32).'...';
+        }
+
+        echo "    <tr valign=\"middle\"" . ($alt ? ' class="alternate"' : '') . ">\n";
+        echo "        <td><strong>{$link->link_name}</strong><br />" . $link->link_description . "</td>\n";
+        echo "        <td><a href=\"{$link->link_url}\" title=\"".sprintf(__('Visit %s'), $link->link_name)."\">{$short_url}</a></td>\n";
+        echo "        <td align=\"center\"><input type=\"checkbox\" name=\"linkcheck[]\" value=\"{$link->link_id}\"" . (isset($uNofollowLinks[$link->link_id]) ? ' checked="checked"' : '') . " /></td>\n";
+        echo "    </tr>\n";
+
+        $alt = !$alt;
+    }
+    ?>
+    </tbody>
+    </table>
+
+    <p class="submit"><input type="submit" class="button" name="nofollowbookmarks" id="nofollowbookmarks" value="Mark Links Nofollow &raquo;" /></p>
+    </form>
+
+    </div>
+    <?php
+}
+
+function nofollow_links_get_bookmarks($links, $args)
+{
+    $sNofollowLinks = get_option("nofollow_links");
+    if (!$sNofollowLinks) {
+        $sNofollowLinks = serialize(array());
+    }
+    $uNofollowLinks = unserialize($sNofollowLinks);
+
+    foreach (array_keys($links) as $i) {
+        if (isset($uNofollowLinks[$links[$i]->link_id])) {
+            $links[$i]->link_rel .= ' nofollow';
+            $links[$i]->link_rel = trim($links[$i]->link_rel);
+        }
+    }
+
+    return $links;
+}
---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Nofollow Links ===
+Contributors: shellab
+Donate link: http://blog.andrewshell.org/nofollow-links
+Tags: nofollow, link, links, blogroll, seo
+Requires at least: 2.1
+Tested up to: 2.3.3
+Stable tag: 1.0.1
+
+Select which links in your blogroll you want to nofollow.
+
+== Description ==
+
+This is a plugin designed to allow you to append nofollow to the rel attribute
+of selected links in your blogroll.
+
+It does not conflict with the link relationships (XFN) specified when you edit
+a link.  It will just be appended to the end of the rel attribute when it's
+displayed.
+
+== Installation ==
+
+1. Upload `nofollow-links` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Navigate to Blogroll > Nofollow Links and select links you want nofollowed
+
+== Frequently Asked Questions ==
+
+No questions yet.
+
+== Screenshots ==
+
+1. Mass edit title tags of pages
+
+== Feedback? ==
+
+Got a bug to report? Or an enhancement to recommend? Or perhaps even some code
+to submit for inclusion in the next release? Great! Share your feedback with
+the author, [Andrew Shell](http://blog.andrewshell.org/contact-andrew).
