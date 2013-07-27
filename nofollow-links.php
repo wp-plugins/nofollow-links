7c62e0718429eb82633f4f263bdddf2ca30b17c9
shellab <shellab@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-27 21:15:41 +0000

    updating header tags
    git-svn-id: https://plugins.svn.wordpress.org/nofollow-links/trunk@747272 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nofollow-links.php
+++ nofollow-links.php
@@ -6,10 +6,24 @@ Description: Select which links in your blogroll you want to nofollow.
 Version: 1.0.5
 Author: Andrew Shell
 Author URI: http://blog.andrewshell.org/
+Text Domain: nofollow-links
+Domain Path: /languages/
+License: GPL2
 
-Copyright (c) 2008 Andrew Shell
-Released under the GNU General Public License (GPL)
-http://www.gnu.org/licenses/gpl.txt
+Copyright (c) 2013 Andrew Shell  (email : andrew@andrewshell.org)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 // Define
